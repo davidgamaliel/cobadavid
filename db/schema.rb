fe06711a0552877cbea45558888a29a9bc89cf57
davidgamaliel <davidgamaliel.sihombing@gmail.com>
2016-01-07 02:01:29 +0700

    added pins scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160103173945) do
+ActiveRecord::Schema.define(version: 20160106181426) do
+
+  create_table "pins", force: true do |t|
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "tweets", force: true do |t|
     t.string   "message"
